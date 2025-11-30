0ab5225d2a9eac580827d298de2a5545d85a80d0
Akash <akash@Akashs-MacBook-Pro.local>
2025-11-30 17:21:53 +0530

    Configure redirects for Samagari Items
    - Redirect to index page after creating a new item
    - Redirect to index page after editing an item

--- app/Filament/Resources/SamagariItemResource/Pages/CreateSamagariItem.php
+++ app/Filament/Resources/SamagariItemResource/Pages/CreateSamagariItem.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateSamagariItem extends CreateRecord
 {
     protected static string $resource = SamagariItemResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
